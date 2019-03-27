d5ef487a83b73b53bace4ef36bf8d923e28fb681
ShevRuslan <sasa02sasa@mail.ru>
2019-03-27 19:05:15 +0600

    Add header (app-navbar)

---
+++ src/components/app/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AppNavbar from '../app-navbar'
+//simple component
+// const App = () => {
+//     return (
+//         <div>1</div>
+//     )
+// }
+
+export default class App extends Component {
+    render () {
+        return (
+            <AppNavbar></AppNavbar>
+        )
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import App from './components/app'
 
-
-const el = <div>Hello World</div>
-
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
