d21316edf04d8f1063149c61c3f4ee270e50c1b7
ShevRuslan <sasa02sasa@mail.ru>
2019-03-29 20:29:36 +0600

    Updates

---
+++ precache-manifest.e922ca5eb86160b87385a810d2386470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de878854dde184d16cd8",
+    "url": "/react-todo/static/css/main.535cc96c.chunk.css"
+  },
+  {
+    "revision": "de878854dde184d16cd8",
+    "url": "/react-todo/static/js/main.3b7d5057.chunk.js"
+  },
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "7d79ad9ee5e9a941c538",
+    "url": "/react-todo/static/js/2.524cc198.chunk.js"
+  },
+  {
+    "revision": "befe13d953e83af9357eb1aa961942fb",
+    "url": "/react-todo/index.html"
+  }
+];
